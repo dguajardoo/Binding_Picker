d0d559d8abee011bdd972d0ac3a7aa92d9f405cd
David <david.guajardo.o@gmail.com>
2017-05-21 23:57:03 -0400

    binding picker

---
+++ Binding/BindingPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using Xamarin.Forms;
+
+namespace Binding
+{
+    public partial class BindingPage : ContentPage
+    {
+        public BindingPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new DetailsViewModel();
+        }
+    }
+
+
+
+
+
+	public class DetailsViewModel : INotifyPropertyChanged
+	{
+		string forename, surname;
+
+		public string Forename
+		{
+			get
+			{
+                Debug.WriteLine(forename);
+				return forename;
+			}
+			set
+			{
+				if (forename != value)
+				{
+					forename = value;
+					OnPropertyChanged("Forename");
+				}
+			}
+		}
+
+		public string Surname
+		{
+			get
+			{
+				return surname;
+			}
+			set
+			{
+				if (surname != value)
+				{
+					surname = value;
+					OnPropertyChanged("Surname");
+				}
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged(string propertyName)
+		{
+			var changed = PropertyChanged;
+			if (changed != null)
+			{
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+	}
+}
